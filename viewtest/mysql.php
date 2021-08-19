84a17dfe02be38c22acaa71e24dabc79f95871d6
smallmonkeyas <192080084@qq.com>
2021-08-20 02:33:34 +0800

    first commit

---
+++ viewtest/mysql.php
@@ -0,0 +1,14 @@
+<?php
+$servername = "mysql";
+$username = "root";
+$password = "123456";
+ 
+// 创建连接
+$conn = new mysqli($servername, $username, $password);
+ 
+// 检测连接
+if ($conn->connect_error) {
+    die("连接失败: " . $conn->connect_error);
+} 
+echo "mysql connected successfully!";
+?>
