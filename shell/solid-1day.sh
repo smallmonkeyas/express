79a87512e964b1ef30db39ddc7c31574fb2c52a6
smallmonkeyas <192080084@qq.com>
2021-12-04 14:40:33 +0800

    数据异常后端:创建定时执行docker脚本和增加对象实例和属性服务

---
+++ shell/solid-1day.sh
@@ -0,0 +1,10 @@
+###
+ # @Author       : Chengxin Sun
+ # @Date         : 2021-12-03 00:10:25
+ # @LastEditors  : Chengxin Sun
+ # @LastEditTime : 2021-12-03 00:10:26
+ # @Description  : Do not edit
+ # @FilePath     : /express/shell/solid.sh
+ # @github-name  : scxmonkeyas
+### 
+docker exec -w /code node-solid-polltion-container npm start
---
+++ shell/solid-addobj.sh
@@ -0,0 +1,10 @@
+###
+ # @Author       : Chengxin Sun
+ # @Date         : 2021-12-03 00:11:33
+ # @LastEditors  : Chengxin Sun
+ # @LastEditTime : 2021-12-03 00:11:34
+ # @Description  : Do not edit
+ # @FilePath     : /express/shell/solid-addobj.sh
+ # @github-name  : scxmonkeyas
+### 
+docker exec -w /code node-solid-polltion-container ts-node ./src/main/index_addObj.ts
